b7970ff619d3ee21420ce5e515e0effe4ddc3e87
Dap Moderator <123246207+thebanon@users.noreply.github.com>
2024-01-16 21:57:36 -0500

    Import Favicon

--- notes/index.js
+++ notes/index.js
@@ -1,3 +1,4 @@
-window.onload = function() {
+window.onload = async function() {
     document.getElementById('avatar').src = "/raw/asset/png/github.png";
+    document.getElementById('snippets').classList.add('active');
 }
